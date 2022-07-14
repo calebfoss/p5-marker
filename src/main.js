d8a7faa6850925e27b613413523ef3881157ae95
Caleb Foss <calebfoss@gmail.com>
2022-07-14 16:14:05 -0500

    Remove setup and draw function assignment as well as Class to HTMLElement conversion (moved to Sketch)

--- src/main.js
+++ src/main.js
@@ -2,31 +2,15 @@ import Base from "./modules/base.js";
 import Color from "./modules/color.js";
 import Logic from "./modules/logic.js";
 import Shape from "./modules/shape.js";
-import { camelToSnake } from "./modules/utils.js";
 
 //  Create an HTML element for every class from modules
 [Color, Base, Logic, Shape]
   .map((module) => Object.entries(module).map(([key, value]) => value))
   .flat()
   .forEach((el) => {
-    customElements.define(`p5-${camelToSnake(el.name)}`, el);
+    customElements.define(el.elementName, el);
   });
 
-const sketch = document.querySelector("p5-sketch");
-
 p5.prototype.anchor = function () {
-  translate(...arguments);
-};
-
-window["setup"] = function setup() {
-  createCanvas(sketch.width, sketch.height).parent(sketch);
-};
-
-window["draw"] = function draw() {
-  Function(sketch.codeString("\t"))();
-  for (let i = 0; i < sketch.children.length; i++) {
-    if (sketch.children[i].hasAttribute("self-destruct")) {
-      sketch.children[i].remove();
-    }
-  }
+  this.translate(...arguments);
 };
