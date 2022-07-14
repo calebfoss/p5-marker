b60ef0f5a315282d7a33d012ffe9a70f69294559
Caleb Foss <calebfoss@gmail.com>
2022-07-14 16:06:46 -0500

    Implement new features on Iterate: - generate for loop from count argument only -  if init is not provided, use assignment vals - rename "test" to "cond"

--- src/modules/logic.js
+++ src/modules/logic.js
@@ -9,10 +9,32 @@ const ifElement = class If extends P5BlockStarter {
 export default [
   class Iterate extends P5BlockStarter {
     constructor() {
-      super(["test", "init, test, update"]);
+      super(["count", "cond", "[init], cond, update"]);
+
+      //  If cond and update provided but not init, use assignment vals as init
+      if (this.update && !this.init) {
+        this.init = this.vals.length
+          ? "let " + this.vals.map((v) => `${v} = ${this[v]}`).join(", ")
+          : "";
+        this.vals = [];
+        this.params.unshift("init");
+      }
+
+      //  Convert count argument to for loop
+      if (this.params[0] === "count") {
+        let charCode = "i".charCodeAt(0);
+        while (typeof window[String.fromCharCode(charCode)] !== "undefined")
+          charCode++;
+        const varName = String.fromCharCode(charCode);
+        Object.defineProperty(this, "fnStr", {
+          get() {
+            return `for(let ${varName} = 0; ${varName} < ${this.count}; ${varName}++) {`;
+          },
+        });
+      }
     }
     get fnName() {
-      if (this.params[0] === "test") return "while";
+      if (this.params[0] === "cond") return "while";
       return "for";
     }
   },
