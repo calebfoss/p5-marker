9c8f1eb7175dcc24047142e3ac9629b7d40984a4
Caleb Foss <calebfoss@gmail.com>
2022-07-19 13:50:49 -0500

    Create get/setters for fill_color & stroke_color Add NONE constant Add Clear element

--- src/modules/color.js
+++ src/modules/color.js
@@ -1,5 +1,7 @@
 import { P5Function } from "./base.js";
 
+p5.prototype.NONE = "none";
+
 class P5ColorFunction extends P5Function {
   constructor(overloads) {
     overloads = [
@@ -7,18 +9,45 @@ class P5ColorFunction extends P5Function {
       "value",
       "gray, [alpha]",
       "values",
-      "color",
+      "c",
       ...overloads,
     ];
     super(overloads);
   }
 }
 
+p5.prototype._gettersAndSetters.push(
+  {
+    name: "fill_color",
+    get: function () {
+      return this.drawingContext.fillStyle;
+    },
+    set: function (val) {
+      if (val === this.NONE) this.noFill();
+      else this.fill(val);
+    },
+  },
+  {
+    name: "stroke_color",
+    get: function () {
+      return this.drawingContext.strokeStyle;
+    },
+    set: function (val) {
+      if (val === this.NONE) this.noStroke();
+      else this.stroke(val);
+    },
+  }
+);
+
 export default [
   class Background extends P5ColorFunction {
     constructor() {
-      const overloads = ["colorstring, [a]", "gray, [a]", "v1, v2, v3, [a]"];
-      super(overloads);
+      super(["colorstring, [a]", "gray, [a]", "v1, v2, v3, [a]"]);
+    }
+  },
+  class Clear extends P5Function {
+    constructor() {
+      super(["", "r, g, b, a"]);
     }
   },
 ];
