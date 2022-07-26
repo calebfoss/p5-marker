7632783cdb688f3d5f54f843799a4c2530719d44
Caleb Foss <calebfoss@gmail.com>
2022-07-26 16:44:21 -0500

    Add kebab case coverters Remove arrays of settings and transformations

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,36 +1,14 @@
 //  casing converters
-export const camelToSnake = (camelStr) =>
+export const camelToKebab = (camelStr) =>
   camelStr.replace(/(?<!^)[A-Z]/g, (letter) => "-" + letter).toLowerCase();
+export const kebabToCamel = (kebabStr) =>
+  kebabStr.replace(/-./g, (s) => s[1].toUpperCase());
+export const pascalToCamel = (pascalStr) =>
+  pascalStr.slice(0, 1).toLowerCase() + pascalStr.slice(1);
+export const pascalToSnake = (pascalStr) =>
+  pascalToCamel(pascalStr).replaceAll(/[A-Z]/g, (c) => "_" + c.toLowerCase());
 export const snakeToCamel = (snakeStr) =>
-  snakeStr.replace(/-./g, (s) => s[1].toUpperCase());
-
-//  functions that set style, modes, etc.
-export const allSettings = [
-  "colorMode",
-  "erase",
-  "noErase",
-  "fill",
-  "noFill",
-  "noStroke",
-  "stroke",
-  "ellipseMode",
-  "noSmooth",
-  "rectMode",
-  "smooth",
-  "strokeCap",
-  "strokeJoin",
-  "strokeWeight",
-  "angleMode",
-];
-
-// transformation functions
-export const transforms = [
-  "anchor",
-  "rotate",
-  "rotateX",
-  "rotateY",
-  "rotateZ",
-  "scale",
-  "shearX",
-  "shearY",
-];
+  snakeStr
+    .split("_")
+    .map((s, i) => (i > 0 ? s.slice(0, 1).toUpperCase() + s.slice(1) : s))
+    .join("");
