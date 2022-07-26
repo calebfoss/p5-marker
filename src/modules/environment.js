59fd429edd61f0fc8e1dcde811dbb61cf9d7864c
Caleb Foss <calebfoss@gmail.com>
2022-07-26 17:22:28 -0500

    Add getter/setter properties Switch export to _defineProperties

--- src/modules/environment.js
+++ src/modules/environment.js
@@ -1,6 +1,6 @@
 import { P5Function } from "./base";
 
-export default [
+p5.prototype._registerElements(
   class Describe extends P5Function {
     constructor() {
       super(["text, [display]"]);
@@ -26,14 +26,103 @@ export default [
       super(["contents"]);
     }
   },
-  class Cursor extends P5Function {
-    constructor() {
-      super(["type, [x], [y]"]);
-    }
-  },
   class FrameRate extends P5Function {
     constructor() {
       super(["", "fps"]);
     }
+  }
+);
+
+p5.prototype._setCursor = p5.prototype.cursor;
+
+p5.prototype.window_resized = false;
+p5.prototype._onresizebase = p5.prototype._onresize;
+p5.prototype._onresize = function (e) {
+  this._onresizebase(e);
+  this._setProperty("window_resized", true);
+};
+
+p5.prototype.registerMethod("post", function () {
+  this._setProperty("window_resized", false);
+});
+
+p5.prototype._fullscreen = p5.prototype.fullscreen;
+
+p5.prototype._defineProperties({
+  cursor: {
+    get: function () {
+      return false;
+      return this._curElement.elt.style.cursor;
+    },
+    set: function (val) {
+      if (typeof val === "array") return this._setCursor(...val);
+      return this._setCursor(val);
+    },
   },
-];
+  frame_count: {
+    get: function () {
+      return this.frameCount;
+    },
+  },
+  delta_time: {
+    get: function () {
+      return this.deltaTime;
+    },
+  },
+  display_width: {
+    get: function () {
+      return this.displayWidth;
+    },
+  },
+  display_height: {
+    get: function () {
+      return this.displayHeight;
+    },
+  },
+  window_width: {
+    get: function () {
+      return this.windowWidth;
+    },
+  },
+  window_height: {
+    get: function () {
+      return this.windowHeight;
+    },
+  },
+  fullscreen: {
+    get: function () {
+      return this._fullscreen();
+    },
+    set: function (val) {
+      this._fullscreen(val);
+    },
+  },
+  pixel_density: {
+    get: function () {
+      return this.pixelDensity();
+    },
+    set: function (val) {
+      this.pixelDensity(val);
+    },
+  },
+  display_density: {
+    get: function () {
+      return this.displayDensity();
+    },
+  },
+  url: {
+    get: function () {
+      return this.getURL();
+    },
+  },
+  url_path: {
+    get: function () {
+      return this.getURLPath();
+    },
+  },
+  url_params: {
+    get: function () {
+      return this.getURLParams();
+    },
+  },
+});
