e0abb87f995e3811229124150a15557b90ed3eb8
Caleb Foss <calebfoss@gmail.com>
2022-07-26 20:02:46 -0500

    Complete p5 color items

--- src/modules/color.js
+++ src/modules/color.js
@@ -2,7 +2,7 @@ import { P5Function } from "./base.js";
 
 p5.prototype.NONE = "none";
 
-class P5ColorFunction extends P5Function {
+class ColorFunction extends P5Function {
   constructor(overloads) {
     overloads = [
       "v1, v2, v3, [alpha]",
@@ -18,6 +18,7 @@ class P5ColorFunction extends P5Function {
 
 p5.prototype.setFill = p5.prototype.fill;
 p5.prototype.setStroke = p5.prototype.stroke;
+p5.prototype.setErase = p5.prototype.erase;
 p5.prototype._defineProperties({
   fill: {
     get: function () {
@@ -37,17 +38,87 @@ p5.prototype._defineProperties({
       else this.setStroke(val);
     },
   },
+  erase: {
+    get: function () {
+      return this._isErasing;
+    },
+    set: function (val) {
+      if (val === true) this.setErase();
+      if (typeof val === "array") this.setErase(...val);
+      this.setErase(val);
+    },
+  },
 });
 
-export default [
-  class Background extends P5ColorFunction {
+p5.prototype._registerElements(
+  class Alpha extends P5Function {
     constructor() {
-      super(["colorstring, [a]", "gray, [a]", "v1, v2, v3, [a]"]);
+      super(["c"]);
     }
+    returnsVal = true;
+  },
+  class Blue extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
+  },
+  class Brightness extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
+  },
+  class Color extends ColorFunction {
+    constructor() {
+      super([]);
+    }
+    returnsVal = true;
+  },
+  class Green extends P5Function {
+    constructor() {
+      super([]);
+    }
+    returnsVal = true;
+  },
+  class Hue extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
+  },
+  class LerpColor extends P5Function {
+    constructor() {
+      super(["c1, c2, amt"]);
+    }
+    returnsVal = true;
+  },
+  class Lightness extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
+  },
+  class Red extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
+  },
+  class Saturation extends P5Function {
+    constructor() {
+      super(["c"]);
+    }
+    returnsVal = true;
   },
   class Clear extends P5Function {
     constructor() {
       super(["", "r, g, b, a"]);
     }
   },
-];
+  class Background extends ColorFunction {
+    constructor() {
+      super(["colorstring, [a]", "gray, [a]", "v1, v2, v3, [a]"]);
+    }
+  }
+);
