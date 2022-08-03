be62dc2703d4fa5ae6d1097608fce15ce4df07ea
Caleb Foss <calebfoss@gmail.com>
2022-08-03 17:00:02 -0500

    Retain setup transformation, fix bugs

--- src/modules/transform.js
+++ src/modules/transform.js
@@ -1,43 +1,45 @@
-p5.prototype._defaultAnchor = p5.prototype.createVector();
-p5.prototype._anchorStack = [p5.prototype._defaultAnchor];
+const defaultAnchor = new p5.Vector();
+p5.prototype._anchorStack = [defaultAnchor.copy()];
 
-p5.prototype._defaultAngle = p5.prototype.createVector();
-p5.prototype._angleStack = [p5.prototype._defaultAngle];
+const defaultAngle = p5.prototype.createVector();
+p5.prototype._angleStack = [defaultAngle.copy()];
 
-p5.prototype._defaultScale = p5.prototype.createVector(1);
-p5.prototype._scaleStack = [p5.prototype._defaultScale];
+const defaultScale = p5.prototype.createVector(1, 1, 1);
+p5.prototype._scaleStack = [defaultScale.copy()];
 
 p5.prototype._pushBase = p5.prototype.push;
 p5.prototype.push = function () {
-  this._anchorStack.push(this._defaultAnchor);
-  this._angleStack.push(this._defaultAngle);
-  this._scaleStack.push(this._defaultScale);
+  this._setProperty(
+    "_anchorStack",
+    this._anchorStack.concat(defaultAnchor.copy())
+  );
+  this._setProperty(
+    "_angleStack",
+    this._angleStack.concat(defaultAngle.copy())
+  );
+  this._setProperty(
+    "_scaleStack",
+    this._scaleStack.concat(defaultScale.copy())
+  );
   this._pushBase();
 };
 
 p5.prototype._popBase = p5.prototype.pop;
 p5.prototype.pop = function () {
-  this._anchorStack.pop();
-  this._angleStack.pop();
-  this._scaleStack.pop();
+  this._setProperty("_anchorStack", this._anchorStack.slice(0, -1));
+  this._setProperty("_angleStack", this._angleStack.slice(0, -1));
+  this._setProperty("_scaleStack", this._scaleStack.slice(0, -1));
   this._popBase();
 };
-
-p5.prototype.registerMethod("pre", function () {
-  this._setProperty("_anchorStack", [this._defaultAnchor]);
-  this._setProperty("_angleStack", [this._defaultAngle]);
-  this._setProperty("_scaleStack", [this._defaultScale]);
-});
-
-p5.prototype.setScale = p5.prototype.scale;
 
 p5.prototype._defineProperties({
   anchor: {
     get: function () {
-      return this._anchorStack.slice(-1)[0];
+      return this._anchorStack[this._anchorStack.length - 1];
     },
     set: function (val) {
-      if (typeof val === "array")
+      const len = this._anchorStack.length;
+      if (Array.isArray(val))
         this._anchorStack[this._anchorStack.length - 1].set(...val);
       else this._anchorStack[this._anchorStack.length - 1].set(val);
       this.translate(this.anchor);
@@ -79,7 +81,15 @@ p5.prototype._defineProperties({
       this.rotateZ(this.angle_z);
     },
   },
-  scale: {
+  angle_vector: {
+    get: function () {
+      return this._angleStack.slice(-1)[0];
+    },
+    set: function (val) {
+      this._angleStack[this._angleStack.length - 1] = val;
+    },
+  },
+  scale_factor: {
     get: function () {
       return this._scaleStack.slice(-1)[0];
     },
@@ -87,7 +97,16 @@ p5.prototype._defineProperties({
       if (typeof val === "array")
         this._scaleStack[this._scaleStack.length - 1].set(...val);
       else this._scaleStack[this._scaleStack.length - 1].set(val);
-      this.scale(this.scaling);
+      this.scale(this.scale_factor);
     },
   },
+});
+
+p5.prototype.registerMethod("pre", function () {
+  this._setProperty("_anchorStack", [this.anchor]);
+  this.translate(this.anchor);
+  this.angle_vector = this.angle_vector;
+  this._setProperty("_angleStack", [this.angle_vector.copy()]);
+  this.scale_factor = this.scale_factor;
+  this._setProperty("_scaleStack", [this.scale_factor.copy()]);
 });
