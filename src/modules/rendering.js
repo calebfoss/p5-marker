79ee80658461e07adc0be9f922b4e8b697626f8e
Caleb Foss <calebfoss@gmail.com>
2022-08-06 14:06:20 -0500

    Complete module, add: - create_canvas - create_graphics - drawing_context - set_drawing_context_prop - set_webgl_attr

--- src/modules/rendering.js
+++ src/modules/rendering.js
@@ -1,3 +1,6 @@
+p5.prototype.create_canvas = p5.prototype.createCanvas;
+p5.prototype.create_graphics = p5.prototype.createGraphics;
+
 p5.prototype._defineProperties({
   blend_mode: {
     get: function () {
@@ -6,6 +9,27 @@ p5.prototype._defineProperties({
     },
     set: function (val) {
       this.blendMode(val);
+    },
+  },
+  drawing_context: {
+    get: function () {
+      return this.drawingContext;
+    },
+  },
+  set_drawing_context_prop: {
+    set: function () {
+      const [arg] = arguments;
+      if (Array.isArray(arg)) {
+        const [prop, val] = arg;
+        this.set_obj_prop = [this.drawingContext, prop, val];
+      } else {
+        this.set_obj_prop = [this.drawingContext, arg];
+      }
+    },
+  },
+  set_webgl_attr: {
+    set: function (val) {
+      this.setAttributes(val);
     },
   },
 });
