085113e3aca826cb0284394367d963dab47f3883
Caleb Foss <calebfoss@gmail.com>
2022-08-14 13:27:44 -0500

    Add storage proxy

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,9 +1,22 @@
 p5.prototype._defineSnakeAlias(
-  "storeItem",
-  "getItem",
-  "clearStorage",
-  "removeItem",
   "createStringDict",
   "createNumberDict",
   "splitTokens"
 );
+p5.prototype.storage = {};
+p5.prototype.registerMethod("init", function () {
+  this._setProperty(
+    "storage",
+    new Proxy(this, {
+      get(target, prop) {
+        if (prop === "clear") return target.clearStorage;
+        if (prop === "remove") return target.removeItem;
+        return target.getItem(prop);
+      },
+      set(target, prop, val) {
+        target.storeItem(prop, val);
+        return true;
+      },
+    })
+  );
+});
