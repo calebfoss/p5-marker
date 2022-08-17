dd620943ee7d4097682fb3fb94b00972cc260f7c
Caleb Foss <calebfoss@gmail.com>
2022-08-17 13:06:38 -0500

    Rename to p5-marker, add min target,... add github url to comment

--- utils/addBuildComment.mjs
+++ utils/addBuildComment.mjs
@@ -1,11 +1,13 @@
 import { readFile, writeFile } from "fs/promises";
 const pkg = JSON.parse(await readFile("./package.json", "utf-8"));
-const build = await readFile(pkg.main, "utf-8");
-const buildCommentStripped =
-  build.slice(0, 2) === "//" ? build.slice(build.indexOf("\n") + 1) : build;
-await writeFile(
-  pkg.main,
-  `// p-dash v${
-    pkg.version
-  } ${new Date().toDateString()}\n${buildCommentStripped}`
-);
+[pkg.main, pkg.min].forEach(async (fileName) => {
+  const build = await readFile(fileName, "utf-8");
+  const buildCommentStripped =
+    build.slice(0, 2) === "//" ? build.slice(build.indexOf("\n") + 1) : build;
+  await writeFile(
+    fileName,
+    `// ${pkg.name} v${pkg.version} ${new Date().toDateString()} ${
+      pkg.repository.url
+    }\n${buildCommentStripped} `
+  );
+});
