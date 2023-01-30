5aa10e650142ca93c3490ede517052aa21a3d69c
calebfoss <calebfoss@gmail.com>
2023-01-30 12:47:41 -0600

    Add width/height to text elements

--- src/elements/typography_elements.js
+++ src/elements/typography_elements.js
@@ -2,7 +2,7 @@ import { RenderedElement } from "../core";
 import { addWebGLMethods } from "../methods/3d_methods";
 import { add3DProps } from "../properties/3d_props";
 import { addFillStroke } from "../properties/color_props";
-import { addXY } from "../properties/shape_props";
+import { addWidthHeight, addXY } from "../properties/shape_props";
 import {
   add2DTransformProps,
   add3DTransformProps,
@@ -10,19 +10,23 @@ import {
 import { addTypographyProps } from "../properties/typography_props";
 
 class Text extends addXY(
-  addFillStroke(addTypographyProps(add2DTransformProps(RenderedElement)))
+  addWidthHeight(
+    addFillStroke(addTypographyProps(add2DTransformProps(RenderedElement)))
+  )
 ) {
-  static overloads = ["content, x, y, [x2], [y2]"];
+  static overloads = ["content, x, y, [width], [height]"];
 }
 customElements.define("p-text", Text);
 
 class Text3D extends addXY(
-  addFillStroke(
-    addTypographyProps(
-      add3DTransformProps(add3DProps(addWebGLMethods(RenderedElement)))
+  addWidthHeight(
+    addFillStroke(
+      addTypographyProps(
+        add3DTransformProps(add3DProps(addWebGLMethods(RenderedElement)))
+      )
     )
   )
 ) {
-  static overloads = ["content, x, y, [x2], [y2]"];
+  static overloads = ["content, x, y, [width], [height]"];
 }
 customElements.define("p-text-3d", Text3D);
