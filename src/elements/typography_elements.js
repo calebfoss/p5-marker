d2ee2a2ded22421299d5b08ea3e3d984ef3d29bb
calebfoss <calebfoss@gmail.com>
2023-01-30 12:06:08 -0600

    Add transform props to text element and add text-3d

--- src/elements/typography_elements.js
+++ src/elements/typography_elements.js
@@ -1,9 +1,28 @@
 import { RenderedElement } from "../core";
+import { addWebGLMethods } from "../methods/3d_methods";
+import { add3DProps } from "../properties/3d_props";
 import { addFillStroke } from "../properties/color_props";
 import { addXY } from "../properties/shape_props";
+import {
+  add2DTransformProps,
+  add3DTransformProps,
+} from "../properties/transform_props";
 import { addTypographyProps } from "../properties/typography_props";
 
-class Text extends addXY(addFillStroke(addTypographyProps(RenderedElement))) {
+class Text extends addXY(
+  addFillStroke(addTypographyProps(add2DTransformProps(RenderedElement)))
+) {
   static overloads = ["content, x, y, [x2], [y2]"];
 }
 customElements.define("p-text", Text);
+
+class Text3D extends addXY(
+  addFillStroke(
+    addTypographyProps(
+      add3DTransformProps(add3DProps(addWebGLMethods(RenderedElement)))
+    )
+  )
+) {
+  static overloads = ["content, x, y, [x2], [y2]"];
+}
+customElements.define("p-text-3d", Text3D);
