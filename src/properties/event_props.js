120b7c4f6c88a7cc4a0942a972615aeba3ba5918
calebfoss <calebfoss@gmail.com>
2023-02-20 12:01:30 -0600

    Document mouse

--- src/properties/event_props.js
+++ src/properties/event_props.js
@@ -1,5 +1,23 @@
 export const addEventProps = (baseClass) =>
   class extends baseClass {
+    /**
+     * The mouse object contains infromation about the current position
+     * and movement of the mouse:
+     * - mouse.x - x-coordinate relative to upper left of canvas
+     * - mouse.y - y-coordinate relative to upper left of canvas
+     * - mouse.previous.x - x-coordinate in previous frame
+     * - mouse.previous.y - y-coordinate in previous frame
+     * - mouse.window.x - x-coordinate relative to upper left of window
+     * - mouse.window.y - y-coordinate relative to upper left of window
+     * - mouse.window.previous.x - window x-coordinate in previous frame
+     * - mouse.window.previous.y - window y-coordinate in previous frame
+     * - mouse.moved.x - horizontal movement of the mouse since last frame
+     * - mouse.moved.y - vertical movement of the mouse since last frame
+     * - mouse.pressed - boolean that is true while the mouse button is held down
+     * - mouse.button - which mouse button is currently pressed: LEFT, RIGHT, CENTER,
+     *    or 0 for none.
+     * @type {Object}
+     */
     get mouse() {
       return {
         x: this.pInst.mouseX,
@@ -20,6 +38,7 @@ export const addEventProps = (baseClass) =>
           x: this.pInst.movedX,
           y: this.pInst.movedY,
         },
+        pressed: this.pInst.mouseIsPressed,
         button: this.pInst.mouseButton,
       };
     }
