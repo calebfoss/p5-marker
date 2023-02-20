5204e0119e8ec7558e1e0958232c1d2ef5c7d757
calebfoss <calebfoss@gmail.com>
2023-02-20 12:08:45 -0600

    Fix bug in canvas default dimension settting

--- src/methods/canvas_methods.js
+++ src/methods/canvas_methods.js
@@ -44,12 +44,12 @@ export const addCanvasMethods = (baseClass) =>
 
         pInst.setup = function () {
           canvas.setup(pInst, canvas);
-          // Set default dimensions (100, 100)
-          canvas.width = 100;
-          canvas.height = 100;
           //  Set default background to transparent
           canvas.background = pInst.color(0, 0);
           pInst.assignCanvas(canvas, canvas.constructor.renderer);
+          // Set default dimensions (100, 100)
+          canvas.width = 100;
+          canvas.height = 100;
         };
         pInst.draw = function () {
           if (canvas.orbit_control) canvas.pInst.orbitControl();
