3365d5bd19ceb89144df4c0f3a78364dbe2e50cb
Caleb Foss <calebfoss@gmail.com>
2023-08-07 12:09:28 -0500

    Add PI to constants

--- src/mixins/constants.ts
+++ src/mixins/constants.ts
@@ -3,4 +3,5 @@ import { Base } from "../elements/base";
 export const constants = <T extends typeof Base>(baseClass: T) =>
   class extends baseClass {
     static NONE = "none";
+    static PI = Math.PI;
   };
